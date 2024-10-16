70757b5e5f017de92630183cc6a0c936c5ba14a7
lucas_a <lucas_a@sicredi.com.br>
2024-10-16 11:21:42 -0300

    Tratamento de verificação de data e hora

--- Agenda/AgendaRequest.cs
+++ Agenda/AgendaRequest.cs
@@ -1,11 +1,19 @@
+using System.Text.Json.Serialization;
+
 namespace MinimalApi.Agendas{
     
     public record AgendaRequest
 {
     public required string Nome { get; init; }
+
     public required string ClienteNome { get; init; }
+
+    [JsonConverter(typeof(DateOnlyJsonConverter))]
     public required DateOnly DataAgendamento { get; init; }
+
+    [JsonConverter(typeof(TimeOnlyJsonConverter))]
     public required TimeOnly HoraAgendamento { get; init; }
+
     public required bool ServicoConcluido { get; init; }
 }
 }
